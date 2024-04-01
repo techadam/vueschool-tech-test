2558f2cc9f2ef439a5416fcf55b82c1d14e28490
techadam90 <abdulrahman.adamu@receeve.com>
2024-04-01 15:17:48 +0100

    Implement mobile navigation

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -1,50 +1,50 @@
 <template>
-  <header class="py-5 px-6 sm:px-12 text-light-grey text-normal">
-    <nav class="flex justify-between items-center">
-      <div class="flex items-center">
-        <img
-          src="@/assets/icons/bars.svg"
-          class="mr-4 sm:mr-3 inline-block xl:hidden w-[26px]"
-          @click="showNav = !showNav"
-        />
-        <img src="@/assets/images/logo.png" class="w-[170px]" />
-      </div>
+  <div
+    class="bg-brand-black text-light-grey overflow-y-auto fixed z-50 right-0 bottom-0 left-0 top-0 p-6"
+  >
+    <div
+      class="inline-flex justify-center items-center h-[40px] w-[40px] text-[16px] bg-brand-primary text-white rounded-full cursor-pointer mx-6 sm:mx-10"
+      @click="close"
+    >
+      x
+    </div>
 
-      <ul class="hidden xlcustom:flex items-center gap-6">
-        <li>
+    <div class="flex justify-center">
+      <ul>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Courses</span>
             <img src="@/assets/icons/arrow-down.svg" />
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Plans</span>
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Business Service</span>
             <img src="@/assets/icons/arrow-down.svg" />
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Learning Paths</span>
             <img src="@/assets/icons/arrow-down.svg" />
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Hiring</span>
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Articles</span>
           </nuxt-link>
         </li>
-        <li>
+        <li class="mb-6">
           <nuxt-link to="/" class="flex items-end gap-1">
             <span>Events</span>
             <img src="@/assets/icons/arrow-down.svg" />
@@ -56,25 +56,14 @@
           </nuxt-link>
         </li>
       </ul>
-
-      <div class="hidden sm:flex items-center gap-6">
-        <nuxt-link to="/" class="inline-block text-white">Log in</nuxt-link>
-        <button
-          to="/"
-          class="inline-block py-[20px] px-[40px] rounded-[10px] border-2 border-brand-primary"
-        >
-          Sign up
-        </button>
-      </div>
-    </nav>
-  </header>
-
-  <mobile-nav @close="showNav = !showNav" v-if="showNav" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import MobileNav from "./MobileNav.vue";
-import { ref } from "vue";
+const emit = defineEmits(['close'])
 
-const showNav = ref(true);
+const close = () => {
+  emit('close')
+}
 </script>
